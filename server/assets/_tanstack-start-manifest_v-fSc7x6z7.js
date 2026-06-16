f28b05a9510aa68b8135e07c5e832dc422e41f02
mypchub3 <mypchub03@gmail.com>
2026-06-15 19:26:20 -0500

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-fSc7x6z7.js
@@ -0,0 +1,20 @@
+//#region \0tanstack-start-manifest:v
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "C:/Users/USER/oil-bridge-connect/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-Dy0gG35s.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-Dy0gG35s.js"
+		} }]
+	},
+	"/": {
+		filePath: "C:/Users/USER/oil-bridge-connect/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-DRscKdem.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
